e56c9dd26aa067cc1ea8abacfc1fb26e92f515fd
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2026-01-16 00:35:03 +0200

    BT short name moved to Kconfig

--- src/vinkey_ble.h
+++ src/vinkey_ble.h
@@ -7,8 +7,5 @@ void vinkey_ble_init();
 void vinkey_ble_send_report(const uint8_t *report, uint16_t len);
 
 int kb_set_report(const struct device *dev,
-             const uint8_t type, const uint8_t id, const uint16_t len,
-             const uint8_t *const buf);
-
-//todo move to Kconfig
-#define BT_NAME_SHORTENED "ElmVntKbd"
+             uint8_t type, uint8_t id, uint16_t len,
+             const uint8_t * buf);
